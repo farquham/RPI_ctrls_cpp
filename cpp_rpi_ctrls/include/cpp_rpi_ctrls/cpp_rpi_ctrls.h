397c4dbdb9d380aed2102738b60155e63adb38f4
Michael Farquharson <mfarquharson1064@gmail.com>
2024-06-15 10:16:55 -0400

    finished adding the required offboard control stuff, may want to edit to better suit my needs but should work for initial tests

--- cpp_rpi_ctrls/include/cpp_rpi_ctrls/cpp_rpi_ctrls.h
+++ cpp_rpi_ctrls/include/cpp_rpi_ctrls/cpp_rpi_ctrls.h
@@ -39,28 +39,71 @@ namespace RPI {
 	public:
 		rpictrls() : Node("rpi_ctrls")
 		{
+            // required qos settings
             rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 		    auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
-            odom_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/vrpn_mocap/baby_michael/pose", qos,
-            [this](const geometry_msgs::msg::PoseStamped::UniquePtr msg) {
-                pos_[0] = msg->pose.position.x;
-                pos_[1] = msg->pose.position.y;
-                pos_[2] = msg->pose.position.z;
-                quat_.w() = msg->pose.orientation.w;
-                quat_.x() = msg->pose.orientation.x;
-                quat_.y() = msg->pose.orientation.y;
-                quat_.z() = msg->pose.orientation.z;
-            });
-
+            // odom publisher and subscriber
+            odom_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/vrpn_mocap/baby_michael/pose", qos, std::bind(&rpictrls::odom_callback, this, std::placeholders::_1));
             odom_pub_ = this->create_publisher<px4_msgs::msg::VehicleOdometry>("/fmu/in/vehicle_visual_odometry", 10);
 
-            auto timer_callback = [this]() -> void {
+            // offboard control stuff
+            offboard_control_mode_pub_ = this->create_publisher<px4_msgs::msg::OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
+            trajectory_setpoint_pub_ = this->create_publisher<px4_msgs::msg::TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
+            vehicle_command_pub_ = this->create_publisher<px4_msgs::msg::VehicleCommand>("/fmu/in/vehicle_command", 10);
+
+            // drone state subscribers
+            vehicle_attitude_sub_ = this->create_subscription<px4_msgs::msg::VehicleAttitude>("/fmu/out/vehicle_attitude", qos, std::bind(&rpictrls::vehicle_attitude_callback, this, std::placeholders::_1));
+            vehicle_local_position_sub_ = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>("/fmu/out/vehicle_local_position", qos, std::bind(&rpictrls::vehicle_local_position_callback, this, std::placeholders::_1));
+            vehicle_acceleration_sub_ = this->create_subscription<px4_msgs::msg::VehicleAcceleration>("/fmu/out/vehicle_acceleration", qos, std::bind(&rpictrls::vehicle_acceleration_callback, this, std::placeholders::_1));
+            vehicle_angular_velocity_sub_ = this->create_subscription<px4_msgs::msg::VehicleAngularVelocity>("/fmu/out/vehicle_angular_velocity", qos, std::bind(&rpictrls::vehicle_angular_velocity_callback, this, std::placeholders::_1));
+
+            // ext setpoint sub
+            ext_setpoint_sub_ = this->create_subscription<geometry_msgs::msg::Point>("/rpi/in/ext_cmdPoint", qos, std::bind(&rpictrls::ext_setpoint_callback, this, std::placeholders::_1));
+
+            // state publishers
+            pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/rpi/out/vehicle_pose", 10);
+            twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/rpi/out/vehicle_velocity", 10);
+            accel_pub_ = this->create_publisher<geometry_msgs::msg::AccelStamped>("/rpi/out/vehicle_acceleration", 10);
+
+
+            auto odom_timer_callback = [this]() -> void {
                 // what ever code to run every timer iteration
                 publish_odom(pos_, quat_);			
             };
 
-			timer_ = this->create_wall_timer(10ms, timer_callback);
+            offboard_setpoint_counter_ = 0;
+
+            auto ob_timer_callback = [this]() -> void {
+                // what ever code to run every timer iteration
+                if (offboard_setpoint_counter_ == 10) {
+                    // Change to Offboard mode after 10 setpoints
+                    this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
+
+                    // Arm the vehicle
+                    this->arm();
+                }
+
+                // offboard_control_mode needs to be paired with trajectory_setpoint
+                publish_obctrl_mode();
+                publish_trajectory_setpoint(pos_cmd_);
+
+                // stop the counter after reaching 11
+                if (offboard_setpoint_counter_ < 11) {
+                    offboard_setpoint_counter_++;
+                }		
+            };
+
+            auto state_timer_callback = [this]() -> void {
+                // what ever code to run every timer iteration
+                publish_state(pos_local, quat_local, avel_local, accel_local);			
+            };
+
+			odom_timer_ = this->create_wall_timer(10ms, odom_timer_callback);
+            ob_timer_ = this->create_wall_timer(10ms, ob_timer_callback);
+            state_timer_ = this->create_wall_timer(10ms, state_timer_callback);
 		}
+        void arm();
+        void disarm();
 
 	private:
 		// needed optitrack stuff
@@ -83,14 +126,38 @@ namespace RPI {
         rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
         rclcpp::Publisher<geometry_msgs::msg::AccelStamped>::SharedPtr accel_pub_;
 
-        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::TimerBase::SharedPtr odom_timer_;
+        rclcpp::TimerBase::SharedPtr ob_timer_;
+        rclcpp::TimerBase::SharedPtr state_timer_;
 
         std::atomic<uint64_t> timestamp_;
-		
+
+        void odom_callback(const geometry_msgs::msg::PoseStamped::UniquePtr & msg);
         void publish_odom(const Eigen::Vector3d pos, const Eigen::Quaterniond quat);
+
+        void publish_vehicle_command(uint16_t command, float param1, float param2);
+        void publish_obctrl_mode();
+        void publish_trajectory_setpoint(const Eigen::Vector3d pos);
+        uint64_t offboard_setpoint_counter_;
+
+        void vehicle_attitude_callback(const px4_msgs::msg::VehicleAttitude::UniquePtr & msg);
+        void vehicle_local_position_callback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr & msg);
+        void vehicle_acceleration_callback(const px4_msgs::msg::VehicleAcceleration::UniquePtr & msg);
+        void vehicle_angular_velocity_callback(const px4_msgs::msg::VehicleAngularVelocity::UniquePtr & msg);
+
+        void ext_setpoint_callback(const geometry_msgs::msg::Point::UniquePtr & msg);
+
+        void publish_state(const Eigen::Vector3d poslocal, const Eigen::Quaterniond quatlocal, const Eigen::Vector3d avellocal, const Eigen::Vector3d accellocal);
 
         Eigen::Vector3d pos_;
         Eigen::Quaterniond quat_;
+
+        Eigen::Vector3d pos_cmd_;
+
+        Eigen::Vector3d pos_local;
+        Eigen::Quaterniond quat_local;
+        Eigen::Vector3d avel_local;
+        Eigen::Vector3d accel_local;
 	};
 }
 
--- cpp_rpi_ctrls/src/cpp_rpi_ctrls.cpp
+++ cpp_rpi_ctrls/src/cpp_rpi_ctrls.cpp
@@ -1,5 +1,17 @@
 #include "../include/cpp_rpi_ctrls/cpp_rpi_ctrls.h"
 
+void RPI::rpictrls::odom_callback(const geometry_msgs::msg::PoseStamped::UniquePtr & msg) {
+    this->pos_[0] = msg->pose.position.x;
+    this->pos_[1] = msg->pose.position.y;
+    this->pos_[2] = msg->pose.position.z;
+    this->quat_.w() = msg->pose.orientation.w;
+    this->quat_.x() = msg->pose.orientation.x;
+    this->quat_.y() = msg->pose.orientation.y;
+    this->quat_.z() = msg->pose.orientation.z;
+    RCLCPP_INFO(this->get_logger(), "Received odom pose: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f", this->pos_[0], this->pos_[1], this->pos_[2], this->quat_.w(), this->quat_.x(), this->quat_.y(), this->quat_.z());
+}
+
+
 void RPI::rpictrls::publish_odom(const Eigen::Vector3d pos, const Eigen::Quaterniond quat) {
     auto odom_msg = px4_msgs::msg::VehicleOdometry();
     odom_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
@@ -14,4 +26,109 @@ void RPI::rpictrls::publish_odom(const Eigen::Vector3d pos, const Eigen::Quatern
     odom_msg.position_variance = {0.0, 0.0, 0.0};
     odom_msg.orientation_variance = {0.0, 0.0, 0.0};
     odom_pub_->publish(odom_msg);
+    RCLCPP_INFO(this->get_logger(), "Published odom: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f", pos[0], pos[2], -1*pos[1], quat.w(), quat.x(), quat.z(), -1*quat.y());
+}
+
+void RPI::rpictrls::arm() {
+    this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1, 0);
+    RCLCPP_INFO(this->get_logger(), "Arming vehicle");
+}
+
+void RPI::rpictrls::disarm() {
+    this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0, 0);
+    RCLCPP_INFO(this->get_logger(), "Disarming vehicle");
+}
+
+void RPI::rpictrls::publish_vehicle_command(uint16_t command, float param1, float param2) {
+    auto vehicle_command_msg = px4_msgs::msg::VehicleCommand();
+    vehicle_command_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    vehicle_command_msg.command = command;
+    vehicle_command_msg.param1 = param1;
+    vehicle_command_msg.param2 = param2;
+    vehicle_command_msg.target_system = 1;
+    vehicle_command_msg.target_component = 1;
+    vehicle_command_msg.source_system = 1;
+    vehicle_command_msg.source_component = 1;
+    vehicle_command_msg.from_external = true;
+    vehicle_command_pub_->publish(vehicle_command_msg);
+}
+
+void RPI::rpictrls::publish_obctrl_mode() {
+    auto offboard_control_mode_msg = px4_msgs::msg::OffboardControlMode();
+    offboard_control_mode_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    offboard_control_mode_msg.position = true;
+    offboard_control_mode_msg.velocity = false;
+    offboard_control_mode_msg.acceleration = false;
+    offboard_control_mode_msg.attitude = false;
+    offboard_control_mode_msg.body_rate = false;
+    offboard_control_mode_pub_->publish(offboard_control_mode_msg);
+}
+
+void RPI::rpictrls::publish_trajectory_setpoint(const Eigen::Vector3d pos) {
+    auto trajectory_setpoint_msg = px4_msgs::msg::TrajectorySetpoint();
+    trajectory_setpoint_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    trajectory_setpoint_msg.position = {float(pos[0]), float(pos[2]), float(-1*pos[1])};
+    trajectory_setpoint_msg.yaw = -3.14/2;
+    trajectory_setpoint_pub_->publish(trajectory_setpoint_msg);
+    RCLCPP_INFO(this->get_logger(), "Published trajectory setpoint: x=%f, y=%f, z=%f", pos[0], pos[1], pos[2]);
+}
+
+void RPI::rpictrls::vehicle_attitude_callback(const px4_msgs::msg::VehicleAttitude::UniquePtr & msg) {
+    this->quat_local.w() = msg->q[0];
+    this->quat_local.x() = msg->q[1];
+    this->quat_local.y() = msg->q[3];
+    this->quat_local.z() = -1*msg->q[2];
+}
+
+void RPI::rpictrls::vehicle_local_position_callback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr & msg) {
+    this->pos_local[0] = msg->x;
+    this->pos_local[1] = msg->z;
+    this->pos_local[2] = -1*msg->y;
+}
+
+void RPI::rpictrls::vehicle_acceleration_callback(const px4_msgs::msg::VehicleAcceleration::UniquePtr & msg) {
+    this->accel_local[0] = msg->xyz[0];
+    this->accel_local[1] = msg->xyz[2];
+    this->accel_local[2] = -1*msg->xyz[1];
+}
+
+void RPI::rpictrls::vehicle_angular_velocity_callback(const px4_msgs::msg::VehicleAngularVelocity::UniquePtr & msg) {
+    this->avel_local[0] = msg->xyz[0];
+    this->avel_local[1] = msg->xyz[2];
+    this->avel_local[2] = -1*msg->xyz[1];
+}
+
+void RPI::rpictrls::ext_setpoint_callback(const geometry_msgs::msg::Point::UniquePtr & msg) {
+    this->pos_cmd_[0] = msg->x;
+    this->pos_cmd_[1] = msg->y;
+    this->pos_cmd_[2] = msg->z;
+}
+
+void RPI::rpictrls::publish_state(const Eigen::Vector3d poslocal, const Eigen::Quaterniond quatlocal, const Eigen::Vector3d avellocal, const Eigen::Vector3d accellocal) {
+    auto pose_msg = geometry_msgs::msg::PoseStamped();
+    pose_msg.header.stamp = this->get_clock()->now();
+    pose_msg.pose.position.x = poslocal[0];
+    pose_msg.pose.position.y = poslocal[1];
+    pose_msg.pose.position.z = poslocal[2];
+    pose_msg.pose.orientation.w = quatlocal.w();
+    pose_msg.pose.orientation.x = quatlocal.x();
+    pose_msg.pose.orientation.y = quatlocal.y();
+    pose_msg.pose.orientation.z = quatlocal.z();
+    pose_pub_->publish(pose_msg);
+
+    auto twist_msg = geometry_msgs::msg::TwistStamped();
+    twist_msg.header.stamp = this->get_clock()->now();
+    twist_msg.twist.linear.x = avellocal[0];
+    twist_msg.twist.linear.y = avellocal[1];
+    twist_msg.twist.linear.z = avellocal[2];
+    twist_pub_->publish(twist_msg);
+
+    auto accel_msg = geometry_msgs::msg::AccelStamped();
+    accel_msg.header.stamp = this->get_clock()->now();
+    accel_msg.accel.linear.x = accellocal[0];
+    accel_msg.accel.linear.y = accellocal[1];
+    accel_msg.accel.linear.z = accellocal[2];
+    accel_pub_->publish(accel_msg);
+
+    RCLCPP_INFO(this->get_logger(), "Published state: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f, avelx=%f, avely=%f, avelz=%f, accelx=%f, accely=%f, accelz=%f", poslocal[0], poslocal[1], poslocal[2], quatlocal.w(), quatlocal.x(), quatlocal.y(), quatlocal.z(), avellocal[0], avellocal[1], avellocal[2], accellocal[0], accellocal[1], accellocal[2]);
 }

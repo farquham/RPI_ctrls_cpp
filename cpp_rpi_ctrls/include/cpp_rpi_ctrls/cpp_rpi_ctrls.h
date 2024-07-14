48938a38fd8ab010bbc54162af56508ffb43398c
Michael Farquharson <mfarquharson1064@gmail.com>
2024-07-13 22:05:54 -0400

    used the px4_com transformation library to transform everything from ros to px4 and vice versa, also used the experimental px4_ros2 library to send the odom data to px4. will need to test next

--- cpp_rpi_ctrls/include/cpp_rpi_ctrls/cpp_rpi_ctrls.h
+++ cpp_rpi_ctrls/include/cpp_rpi_ctrls/cpp_rpi_ctrls.h
@@ -18,103 +18,59 @@
 #include <geometry_msgs/msg/point.hpp>
 
 // px4 ros msgs
-#include <px4_msgs/msg/vehicle_odometry.hpp>
 #include <px4_msgs/msg/vehicle_attitude.hpp>
 #include <px4_msgs/msg/vehicle_local_position.hpp>
 #include <px4_msgs/msg/vehicle_acceleration.hpp>
 #include <px4_msgs/msg/vehicle_angular_velocity.hpp>
-//offboard control
-#include <px4_msgs/msg/offboard_control_mode.hpp>
-#include <px4_msgs/msg/trajectory_setpoint.hpp>
-#include <px4_msgs/msg/vehicle_command.hpp>
+
+// px4 lib stuff
+#include <px4_ros_com/frame_transforms.h>
+#include <px4_ros2/navigation/experimental/local_position_measurement_interface.hpp>
 
 // some nanespace stuff
 using namespace std::chrono;
 using namespace std::chrono_literals;
 typedef std::chrono::high_resolution_clock clocky;
 
-namespace RPI {
-	// class for offboard control which starts a ROS2 node
-	class rpictrls : public rclcpp::Node {
-	public:
-		rpictrls() : Node("rpi_ctrls")
-		{
+
+class LocalNavigation : public px4_ros2::LocalPositionMeasurementInterface {
+    public:
+        explicit LocalNavigation(rclcpp::Node & node): LocalPositionMeasurementInterface(node, px4_ros2::PoseFrame::LocalNED, px4_ros2::VelocityFrame::LocalNED) {
             // required qos settings
             rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 		    auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
             // odom publisher and subscriber
-            odom_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/vrpn_mocap/baby_michael/pose", qos, std::bind(&rpictrls::odom_callback, this, std::placeholders::_1));
-            odom_pub_ = this->create_publisher<px4_msgs::msg::VehicleOdometry>("/fmu/in/vehicle_visual_odometry", 10);
-
-            // offboard control stuff
-            offboard_control_mode_pub_ = this->create_publisher<px4_msgs::msg::OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
-            trajectory_setpoint_pub_ = this->create_publisher<px4_msgs::msg::TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
-            vehicle_command_pub_ = this->create_publisher<px4_msgs::msg::VehicleCommand>("/fmu/in/vehicle_command", 10);
+            odom_sub_ = node.create_subscription<geometry_msgs::msg::PoseStamped>("/vrpn_mocap/baby_michael/pose", qos, std::bind(&LocalNavigation::odom_callback, this, std::placeholders::_1));
 
             // drone state subscribers
-            vehicle_attitude_sub_ = this->create_subscription<px4_msgs::msg::VehicleAttitude>("/fmu/out/vehicle_attitude", qos, std::bind(&rpictrls::vehicle_attitude_callback, this, std::placeholders::_1));
-            vehicle_local_position_sub_ = this->create_subscription<px4_msgs::msg::VehicleLocalPosition>("/fmu/out/vehicle_local_position", qos, std::bind(&rpictrls::vehicle_local_position_callback, this, std::placeholders::_1));
-            vehicle_acceleration_sub_ = this->create_subscription<px4_msgs::msg::VehicleAcceleration>("/fmu/out/vehicle_acceleration", qos, std::bind(&rpictrls::vehicle_acceleration_callback, this, std::placeholders::_1));
-            vehicle_angular_velocity_sub_ = this->create_subscription<px4_msgs::msg::VehicleAngularVelocity>("/fmu/out/vehicle_angular_velocity", qos, std::bind(&rpictrls::vehicle_angular_velocity_callback, this, std::placeholders::_1));
+            vehicle_attitude_sub_ = node.create_subscription<px4_msgs::msg::VehicleAttitude>("/fmu/out/vehicle_attitude", qos, std::bind(&LocalNavigation::vehicle_attitude_callback, this, std::placeholders::_1));
+            vehicle_local_position_sub_ = node.create_subscription<px4_msgs::msg::VehicleLocalPosition>("/fmu/out/vehicle_local_position", qos, std::bind(&LocalNavigation::vehicle_local_position_callback, this, std::placeholders::_1));
+            vehicle_acceleration_sub_ = node.create_subscription<px4_msgs::msg::VehicleAcceleration>("/fmu/out/vehicle_acceleration", qos, std::bind(&LocalNavigation::vehicle_acceleration_callback, this, std::placeholders::_1));
+            vehicle_angular_velocity_sub_ = node.create_subscription<px4_msgs::msg::VehicleAngularVelocity>("/fmu/out/vehicle_angular_velocity", qos, std::bind(&LocalNavigation::vehicle_angular_velocity_callback, this, std::placeholders::_1));
 
             // ext setpoint sub
-            ext_setpoint_sub_ = this->create_subscription<geometry_msgs::msg::Point>("/rpi/in/ext_cmdPoint", qos, std::bind(&rpictrls::ext_setpoint_callback, this, std::placeholders::_1));
+            ext_setpoint_sub_ = node.create_subscription<geometry_msgs::msg::Point>("/rpi/in/ext_cmdPoint", qos, std::bind(&LocalNavigation::ext_setpoint_callback, this, std::placeholders::_1));
 
             // state publishers
-            pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/rpi/out/vehicle_pose", 10);
-            twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/rpi/out/vehicle_velocity", 10);
-            accel_pub_ = this->create_publisher<geometry_msgs::msg::AccelStamped>("/rpi/out/vehicle_acceleration", 10);
-
-
-            auto odom_timer_callback = [this]() -> void {
-                // what ever code to run every timer iteration
-                publish_odom(pos_, quat_);			
-            };
-
-            offboard_setpoint_counter_ = 0;
-
-            auto ob_timer_callback = [this]() -> void {
-                if ((abs(this->pos_cmd_[0]) > 0) || (abs(this->pos_cmd_[1]) > 0) || (abs(this->pos_cmd_[2]) > 0)) {
-                    // what ever code to run every timer iteration
-                    if (offboard_setpoint_counter_ == 10) {
-                        // Change to Offboard mode after 10 setpoints
-                        this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
-
-                        // Arm the vehicle
-                        this->arm();
-                    }
-
-                    // offboard_control_mode needs to be paired with trajectory_setpoint
-                    publish_obctrl_mode();
-                    publish_trajectory_setpoint(pos_cmd_);
-
-                    // stop the counter after reaching 11
-                    if (offboard_setpoint_counter_ < 11) {
-                        offboard_setpoint_counter_++;
-                    }	
-                }	
-            };
+            pose_pub_ = node.create_publisher<geometry_msgs::msg::PoseStamped>("/rpi/out/vehicle_pose", 10);
+            twist_pub_ = node.create_publisher<geometry_msgs::msg::TwistStamped>("/rpi/out/vehicle_velocity", 10);
+            accel_pub_ = node.create_publisher<geometry_msgs::msg::AccelStamped>("/rpi/out/vehicle_acceleration", 10);
 
             auto state_timer_callback = [this]() -> void {
                 // what ever code to run every timer iteration
                 publish_state(pos_local, quat_local, avel_local, accel_local);			
             };
 
-			odom_timer_ = this->create_wall_timer(10ms, odom_timer_callback);
-            ob_timer_ = this->create_wall_timer(10ms, ob_timer_callback);
-            state_timer_ = this->create_wall_timer(10ms, state_timer_callback);
-		}
-        void arm();
-        void disarm();
+            state_timer_ = node.create_wall_timer(10ms, state_timer_callback);
 
-	private:
-		// needed optitrack stuff
+            position_timer = node.create_wall_timer(10ms, [this] {updateLocalPosition();});
+
+            RCLCPP_INFO(node.get_logger(), "example_local_navigation_node running!");
+        }
+
+    private:
+        // needed optitrack stuff
         rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr odom_sub_;
-        rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr odom_pub_;
-        // offboard control stuff
-        rclcpp::Publisher<px4_msgs::msg::OffboardControlMode>::SharedPtr offboard_control_mode_pub_;
-        rclcpp::Publisher<px4_msgs::msg::TrajectorySetpoint>::SharedPtr trajectory_setpoint_pub_;
-        rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr vehicle_command_pub_;
         // drone state subscribers
         rclcpp::Subscription<px4_msgs::msg::VehicleAttitude>::SharedPtr vehicle_attitude_sub_;
         rclcpp::Subscription<px4_msgs::msg::VehicleLocalPosition>::SharedPtr vehicle_local_position_sub_;
@@ -128,19 +84,14 @@ namespace RPI {
         rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
         rclcpp::Publisher<geometry_msgs::msg::AccelStamped>::SharedPtr accel_pub_;
 
-        rclcpp::TimerBase::SharedPtr odom_timer_;
-        rclcpp::TimerBase::SharedPtr ob_timer_;
         rclcpp::TimerBase::SharedPtr state_timer_;
+        rclcpp::TimerBase::SharedPtr position_timer;
 
         std::atomic<uint64_t> timestamp_;
 
         void odom_callback(const geometry_msgs::msg::PoseStamped::UniquePtr & msg);
-        void publish_odom(const Eigen::Vector3d pos, const Eigen::Quaterniond quat);
 
-        void publish_vehicle_command(uint16_t command, float param1, float param2);
-        void publish_obctrl_mode();
-        void publish_trajectory_setpoint(const Eigen::Vector3d pos);
-        uint64_t offboard_setpoint_counter_;
+        void updateLocalPosition();
 
         void vehicle_attitude_callback(const px4_msgs::msg::VehicleAttitude::UniquePtr & msg);
         void vehicle_local_position_callback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr & msg);
@@ -151,8 +102,10 @@ namespace RPI {
 
         void publish_state(const Eigen::Vector3d poslocal, const Eigen::Quaterniond quatlocal, const Eigen::Vector3d avellocal, const Eigen::Vector3d accellocal);
 
-        Eigen::Vector3d pos_;
-        Eigen::Quaterniond quat_;
+        Eigen::Vector3d pos_ros;
+        Eigen::Quaterniond quat_ros;
+        Eigen::Vector3d pos_px4;
+        Eigen::Quaterniond quat_px4;
 
         Eigen::Vector3d pos_cmd_;
 
@@ -160,6 +113,23 @@ namespace RPI {
         Eigen::Quaterniond quat_local;
         Eigen::Vector3d avel_local;
         Eigen::Vector3d accel_local;
+};
+
+namespace RPI {
+	// class for offboard control which starts a ROS2 node
+	class rpictrls : public rclcpp::Node {
+	public:
+		rpictrls() : Node("rpi_ctrls")
+		{
+            _interface = std::make_unique<LocalNavigation>(*this);
+
+            if (!_interface->doRegister()) {
+                throw std::runtime_error("Registration failed");
+            }
+		}
+
+	private:
+		std::unique_ptr<LocalNavigation> _interface;
 	};
 }
 
--- cpp_rpi_ctrls/src/cpp_rpi_ctrls.cpp
+++ cpp_rpi_ctrls/src/cpp_rpi_ctrls.cpp
@@ -1,134 +1,107 @@
 #include "../include/cpp_rpi_ctrls/cpp_rpi_ctrls.h"
 
-void RPI::rpictrls::odom_callback(const geometry_msgs::msg::PoseStamped::UniquePtr & msg) {
-    this->pos_[0] = msg->pose.position.x;
-    this->pos_[1] = -1*msg->pose.position.y;
-    this->pos_[2] = -1* msg->pose.position.z;
-    this->quat_.w() = msg->pose.orientation.w;
-    this->quat_.x() = msg->pose.orientation.x;
-    this->quat_.y() = -1*msg->pose.orientation.y;
-    this->quat_.z() = -1*msg->pose.orientation.z;
-    RCLCPP_INFO(this->get_logger(), "Received odom pose: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f", this->pos_[0], this->pos_[1], this->pos_[2], this->quat_.w(), this->quat_.x(), this->quat_.y(), this->quat_.z());
+void LocalNavigation::odom_callback(const geometry_msgs::msg::PoseStamped::UniquePtr & msg) {
+    this->pos_ros[0] = msg->pose.position.x;
+    this->pos_ros[1] = msg->pose.position.y;
+    this->pos_ros[2] = msg->pose.position.z;
+    this->quat_ros.w() = msg->pose.orientation.w;
+    this->quat_ros.x() = msg->pose.orientation.x;
+    this->quat_ros.y() = msg->pose.orientation.y;
+    this->quat_ros.z() = msg->pose.orientation.z;
+    this->pos_px4 = px4_ros_com::frame_transforms::enu_to_ned_local_frame(this->pos_ros);
+    this->quat_px4 = px4_ros_com::frame_transforms::ros_to_px4_orientation(this->quat_ros);
+    //RCLCPP_INFO(this->get_logger(), "Received odom pose: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f", this->pos_ros[0], this->pos_ros[1], this->pos_ros[2], this->quat_ros.w(), this->quat_ros.x(), this->quat_ros.y(), this->quat_ros.z());
 }
 
+void LocalNavigation::updateLocalPosition() {
+    px4_ros2::LocalPositionMeasurement local_position_measurement {};
 
-void RPI::rpictrls::publish_odom(const Eigen::Vector3d pos, const Eigen::Quaterniond quat) {
-    auto odom_msg = px4_msgs::msg::VehicleOdometry();
-    odom_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    odom_msg.pose_frame = 2;
-    odom_msg.position[0] = pos[0];
-    odom_msg.position[1] = pos[1];
-    odom_msg.position[2] = pos[2];
-    odom_msg.q[0] = quat.w();
-    odom_msg.q[1] = quat.x();
-    odom_msg.q[2] = quat.y();
-    odom_msg.q[3] = quat.z();
-    odom_msg.position_variance = {0.0, 0.0, 0.0};
-    odom_msg.orientation_variance = {0.0, 0.0, 0.0};
-    odom_pub_->publish(odom_msg);
-    RCLCPP_INFO(this->get_logger(), "Published odom: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f", pos[0], pos[1], pos[2], quat.w(), quat.x(), quat.y(), quat.z());
+    local_position_measurement.timestamp_sample = _node.get_clock()->now();
+
+    local_position_measurement.velocity_xy = Eigen::Vector2f {float(this->pos_px4[0]), float(this->pos_px4[1])};
+    // local_position_measurement.velocity_xy_variance = Eigen::Vector2f {0.3f, 0.4f};
+    local_position_measurement.velocity_xy_variance = Eigen::Vector2f {0.0f, 0.0f};
+
+    local_position_measurement.position_z = float(this->pos_px4[2]);
+    // local_position_measurement.position_z_variance = 0.33f;
+    local_position_measurement.position_z_variance = 0.0f;
+
+    local_position_measurement.attitude_quaternion = Eigen::Quaternionf {float(this->quat_px4.w()), float(this->quat_px4.x()), float(this->quat_px4.y()), float(this->quat_px4.z())};
+    // local_position_measurement.attitude_variance = Eigen::Vector3f {0.2f, 0.1f, 0.05f};
+    local_position_measurement.attitude_variance = Eigen::Vector3f {0.0f, 0.0f, 0.0f};
+
+    try {
+        update(local_position_measurement);
+        RCLCPP_DEBUG(_node.get_logger(),"Successfully sent position update to navigation interface.");
+    } catch (const px4_ros2::NavigationInterfaceInvalidArgument & e){
+        RCLCPP_ERROR_THROTTLE(_node.get_logger(), *_node.get_clock(), 1000, "Exception caught: %s", e.what());
+    }
 }
 
-void RPI::rpictrls::arm() {
-    this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1, 0);
-    RCLCPP_INFO(this->get_logger(), "Arming vehicle");
-}
-
-void RPI::rpictrls::disarm() {
-    this->publish_vehicle_command(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0, 0);
-    RCLCPP_INFO(this->get_logger(), "Disarming vehicle");
-}
-
-void RPI::rpictrls::publish_vehicle_command(uint16_t command, float param1, float param2) {
-    auto vehicle_command_msg = px4_msgs::msg::VehicleCommand();
-    vehicle_command_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    vehicle_command_msg.command = command;
-    vehicle_command_msg.param1 = param1;
-    vehicle_command_msg.param2 = param2;
-    vehicle_command_msg.target_system = 1;
-    vehicle_command_msg.target_component = 1;
-    vehicle_command_msg.source_system = 1;
-    vehicle_command_msg.source_component = 1;
-    vehicle_command_msg.from_external = true;
-    vehicle_command_pub_->publish(vehicle_command_msg);
-}
-
-void RPI::rpictrls::publish_obctrl_mode() {
-    auto offboard_control_mode_msg = px4_msgs::msg::OffboardControlMode();
-    offboard_control_mode_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    offboard_control_mode_msg.position = true;
-    offboard_control_mode_msg.velocity = false;
-    offboard_control_mode_msg.acceleration = false;
-    offboard_control_mode_msg.attitude = false;
-    offboard_control_mode_msg.body_rate = false;
-    offboard_control_mode_pub_->publish(offboard_control_mode_msg);
-}
-
-void RPI::rpictrls::publish_trajectory_setpoint(const Eigen::Vector3d pos) {
-    auto trajectory_setpoint_msg = px4_msgs::msg::TrajectorySetpoint();
-    trajectory_setpoint_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    trajectory_setpoint_msg.position = {float(pos[0]), float(pos[1]), float(pos[2])};
-    trajectory_setpoint_msg.yaw = 0.0;
-    trajectory_setpoint_pub_->publish(trajectory_setpoint_msg);
-    RCLCPP_INFO(this->get_logger(), "Published trajectory setpoint: x=%f, y=%f, z=%f", pos[0], pos[1], pos[2]);
-}
-
-void RPI::rpictrls::vehicle_attitude_callback(const px4_msgs::msg::VehicleAttitude::UniquePtr & msg) {
+void LocalNavigation::vehicle_attitude_callback(const px4_msgs::msg::VehicleAttitude::UniquePtr & msg) {
     this->quat_local.w() = msg->q[0];
-    this->quat_local.x() = msg->q[2];
-    this->quat_local.y() = msg->q[1];
-    this->quat_local.z() = -1*msg->q[3];
+    this->quat_local.x() = msg->q[1];
+    this->quat_local.y() = msg->q[2];
+    this->quat_local.z() = msg->q[3];
 }
 
-void RPI::rpictrls::vehicle_local_position_callback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr & msg) {
-    this->pos_local[0] = msg->y;
-    this->pos_local[1] = msg->x;
-    this->pos_local[2] = -1*msg->z;
+void LocalNavigation::vehicle_local_position_callback(const px4_msgs::msg::VehicleLocalPosition::UniquePtr & msg) {
+    this->pos_local[0] = msg->x;
+    this->pos_local[1] = msg->y;
+    this->pos_local[2] = msg->z;
 }
 
-void RPI::rpictrls::vehicle_acceleration_callback(const px4_msgs::msg::VehicleAcceleration::UniquePtr & msg) {
-    this->accel_local[0] = msg->xyz[1];
-    this->accel_local[1] = msg->xyz[0];
-    this->accel_local[2] = -1*msg->xyz[2];
+void LocalNavigation::vehicle_acceleration_callback(const px4_msgs::msg::VehicleAcceleration::UniquePtr & msg) {
+    this->accel_local[0] = msg->xyz[0];
+    this->accel_local[1] = msg->xyz[1];
+    this->accel_local[2] = msg->xyz[2];
 }
 
-void RPI::rpictrls::vehicle_angular_velocity_callback(const px4_msgs::msg::VehicleAngularVelocity::UniquePtr & msg) {
-    this->avel_local[0] = msg->xyz[1];
-    this->avel_local[1] = msg->xyz[0];
-    this->avel_local[2] = -1*msg->xyz[2];
+void LocalNavigation::vehicle_angular_velocity_callback(const px4_msgs::msg::VehicleAngularVelocity::UniquePtr & msg) {
+    this->avel_local[0] = msg->xyz[0];
+    this->avel_local[1] = msg->xyz[1];
+    this->avel_local[2] = msg->xyz[2];
 }
 
-void RPI::rpictrls::ext_setpoint_callback(const geometry_msgs::msg::Point::UniquePtr & msg) {
-    this->pos_cmd_[0] = msg->y;
-    this->pos_cmd_[1] = msg->x;
-    this->pos_cmd_[2] = -1*msg->z;
+void LocalNavigation::ext_setpoint_callback(const geometry_msgs::msg::Point::UniquePtr & msg) {
+    this->pos_cmd_[0] = msg->x;
+    this->pos_cmd_[1] = msg->y;
+    this->pos_cmd_[2] = msg->z;
 }
 
-void RPI::rpictrls::publish_state(const Eigen::Vector3d poslocal, const Eigen::Quaterniond quatlocal, const Eigen::Vector3d avellocal, const Eigen::Vector3d accellocal) {
+void LocalNavigation::publish_state(const Eigen::Vector3d poslocal, const Eigen::Quaterniond quatlocal, const Eigen::Vector3d avellocal, const Eigen::Vector3d accellocal) {
+    Eigen::Vector3d poslocal_ros;
+    Eigen::Quaterniond quatlocal_ros;
+    poslocal_ros = px4_ros_com::frame_transforms::ned_to_enu_local_frame(poslocal);
+    quatlocal_ros = px4_ros_com::frame_transforms::px4_to_ros_orientation(quatlocal);
     auto pose_msg = geometry_msgs::msg::PoseStamped();
-    pose_msg.header.stamp = this->get_clock()->now();
-    pose_msg.pose.position.x = poslocal[0];
-    pose_msg.pose.position.y = poslocal[1];
-    pose_msg.pose.position.z = poslocal[2];
-    pose_msg.pose.orientation.w = quatlocal.w();
-    pose_msg.pose.orientation.x = quatlocal.x();
-    pose_msg.pose.orientation.y = quatlocal.y();
-    pose_msg.pose.orientation.z = quatlocal.z();
+    pose_msg.header.stamp = _node.get_clock()->now();
+    pose_msg.pose.position.x = poslocal_ros[0];
+    pose_msg.pose.position.y = poslocal_ros[1];
+    pose_msg.pose.position.z = poslocal_ros[2];
+    pose_msg.pose.orientation.w = quatlocal_ros.w();
+    pose_msg.pose.orientation.x = quatlocal_ros.x();
+    pose_msg.pose.orientation.y = quatlocal_ros.y();
+    pose_msg.pose.orientation.z = quatlocal_ros.z();
     pose_pub_->publish(pose_msg);
 
+    Eigen::Vector3d avellocal_ros;
+    avellocal_ros = px4_ros_com::frame_transforms::ned_to_enu_local_frame(avellocal);
     auto twist_msg = geometry_msgs::msg::TwistStamped();
-    twist_msg.header.stamp = this->get_clock()->now();
-    twist_msg.twist.linear.x = avellocal[0];
-    twist_msg.twist.linear.y = avellocal[1];
-    twist_msg.twist.linear.z = avellocal[2];
+    twist_msg.header.stamp = _node.get_clock()->now();
+    twist_msg.twist.linear.x = avellocal_ros[0];
+    twist_msg.twist.linear.y = avellocal_ros[1];
+    twist_msg.twist.linear.z = avellocal_ros[2];
     twist_pub_->publish(twist_msg);
 
+    Eigen::Vector3d accellocal_ros;
+    accellocal_ros = px4_ros_com::frame_transforms::ned_to_enu_local_frame(accellocal);
     auto accel_msg = geometry_msgs::msg::AccelStamped();
-    accel_msg.header.stamp = this->get_clock()->now();
-    accel_msg.accel.linear.x = accellocal[0];
-    accel_msg.accel.linear.y = accellocal[1];
-    accel_msg.accel.linear.z = accellocal[2];
+    accel_msg.header.stamp = _node.get_clock()->now();
+    accel_msg.accel.linear.x = accellocal_ros[0];
+    accel_msg.accel.linear.y = accellocal_ros[1];
+    accel_msg.accel.linear.z = accellocal_ros[2];
     accel_pub_->publish(accel_msg);
 
-    RCLCPP_INFO(this->get_logger(), "Published state: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f, avelx=%f, avely=%f, avelz=%f, accelx=%f, accely=%f, accelz=%f", poslocal[0], poslocal[1], poslocal[2], quatlocal.w(), quatlocal.x(), quatlocal.y(), quatlocal.z(), avellocal[0], avellocal[1], avellocal[2], accellocal[0], accellocal[1], accellocal[2]);
+    //RCLCPP_INFO(this->get_logger(), "Published state: x=%f, y=%f, z=%f, qw=%f, qx=%f, qy=%f, qz=%f, avelx=%f, avely=%f, avelz=%f, accelx=%f, accely=%f, accelz=%f", poslocal_ros[0], poslocal_ros[1], poslocal_ros[2], quatlocal_ros.w(), quatlocal_ros.x(), quatlocal_ros.y(), quatlocal_ros.z(), avellocal_ros[0], avellocal_ros[1], avellocal_ros[2], accellocal_ros[0], accellocal_ros[1], accellocal_ros[2]);
 }
